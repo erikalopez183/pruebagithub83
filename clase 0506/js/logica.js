e300ada084fac8e064f4671276d592bbb3f9b1d8
Erika <erikalopez183@gmail.com>
2024-06-10 16:30:43 -0500

    logica para iniciar sesion

--- clase 0506/js/logica.js
+++ clase 0506/js/logica.js
@@ -1,1 +1,20 @@
 console.log('logica conectada');
+
+const usuario = 'Admin'
+const clave = '1234'
+
+
+function pruebaBoton(){
+
+    let user = document.getElementById('user').value
+    let pw = document.getElementById('pw').value
+
+    if(usuario==user && clave==pw){
+        window.open('calculadora.html')
+    }else{
+        console.log('error de credenciales');
+
+    }
+    
+
+}
