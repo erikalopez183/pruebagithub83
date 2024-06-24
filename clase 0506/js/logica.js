0fbbfe8a74eb2f02570184a65fe861573c4471f8
Erika <erikalopez183@gmail.com>
2024-06-23 22:26:17 -0500

    logica2

--- clase 0506/js/logica.js
+++ clase 0506/js/logica.js
@@ -1,28 +1,65 @@
-console.log('logica conectada');
+console.log('logica inicio sesion');
 
-const usuario = [{user:'Admin', clave: '1234'};
-{user:'root', clave:'123'}]
+const usuario = 'admin'
 const clave = '1234'
+let contador =0
 
-const frutas = ['Manzana', 'Pera', 'Sandia']
+let frutas = ['Manzana', 'Pera', 'Sandia']
 
 
 function pruebaBoton(){
 
-    let user = document.getElementById('user').value
+    let user = document.getElementById('user').value.toLocaleUpperCase()
     let pw = document.getElementById('pw').value
 
-    if (usuario==user && clave==pw){
-        window.open('calculadora.html')
+    if (usuario.toLocaleUpperCase()==user && clave==pw){
+        //logueado
+        setUser()
+        window.open('localStorage.html')
     }else{
+
+        bloquearCuenta()
         console.log('error de credenciales');
+    }
+}
+//bloquear el acceso
+function bloquearCuenta(){
+    let contadorStorage =localStorage.getItem('contador') || 0
+    console.log(`valor inicial storage ${contadorStorage}`);
+    if(contadorStorage==3){
+        console.log('cuenta bloqueada');
+    }else{
+        contarIntentos()  
     }
 
 }
+//contar cada vez que se equivoque
+
+function contarIntentos(){
+    let contadorStorage =localStorage.getItem('contador') || 0
+    contadorStorage++
+    console.log(`contador antes de sumar${ contadorStorage}`);
+    setContador(contadorStorage)
+}
+
+function setUser(){
+    localStorage.setItem('usuario',usuario)
+}
+
+function setContador(contador){
+    localStorage.setItem('contador',contador)
+}
+
+function desbloquearCuenta(){
+    localStorage.removeItem('contador')
+}
+
+
 function imprimirArreglo(){
     let posicion= document.getElementById('arrayPosicion').value
     console.log(localStorage.getItem('arregloFrutas'));
-    if (posicion>=frutas.length) {
+
+        if (posicion>=frutas.length) {
         console.log('posicion no valida');
         limpiar()
     }else{
