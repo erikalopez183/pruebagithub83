0783e13a44b12b36836e4c8e961520a4870fea7d
Erika <erikalopez183@gmail.com>
2024-06-23 21:51:26 -0500

    logica

--- clase 0506/js/logica.js
+++ clase 0506/js/logica.js
@@ -1,7 +1,10 @@
 console.log('logica conectada');
 
-const usuario = 'Admin'
+const usuario = [{user:'Admin', clave: '1234'};
+{user:'root', clave:'123'}]
 const clave = '1234'
+
+const frutas = ['Manzana', 'Pera', 'Sandia']
 
 
 function pruebaBoton(){
@@ -9,12 +12,51 @@ function pruebaBoton(){
     let user = document.getElementById('user').value
     let pw = document.getElementById('pw').value
 
-    if(usuario==user && clave==pw){
+    if (usuario==user && clave==pw){
         window.open('calculadora.html')
     }else{
         console.log('error de credenciales');
-
     }
-    
 
+}
+function imprimirArreglo(){
+    let posicion= document.getElementById('arrayPosicion').value
+    console.log(localStorage.getItem('arregloFrutas'));
+    if (posicion>=frutas.length) {
+        console.log('posicion no valida');
+        limpiar()
+    }else{
+        console.log(frutas[posicion]);
+    }
+}
+
+function limpiar(){
+    document.getElementById('arrayPosicion').value =''
+    document.getElementById('arrayPosicion').focus()
+}
+
+function agregarFruta(){
+    let nombreFruta=document.getElementById('nombreFruta').value.toLocaleUpperCase()
+    nombreFruta= nombreFruta.trim()
+    if (nombreFruta.trim()!==''){
+        frutas.push(nombreFruta)
+        guardarStorage(frutas)
+        console.log(frutas);
+        console.log(`Se agrego la fruta ${nombreFruta}`);
+        document.getElementById('nombreFruta').focus()
+    }else{
+        console.log('Nombre no valido');
+    }
+
+}
+function eliminarFruta(){
+    let posicion= document.getElementById('arrayPosicion').value
+    frutas.splice(posicion,1)
+    console.log('eliminar   '+frutas);
+
+}
+
+function guardarStorage(arreglo){
+localStorage.setItem('arregloFrutas',arreglo)
+console.log('guardo en storage');
 }
